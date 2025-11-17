29df30dfac5d5178f0a6654cebd13fc14dfa8db3
DaveyEke <daveydenco@gmail.com>
2025-11-17 09:47:53 +0100

    working

--- src/ExpoSpeechTranscriberModule.web.ts
+++ src/ExpoSpeechTranscriberModule.web.ts
@@ -1,14 +1,14 @@
-import { registerWebModule, NativeModule } from 'expo';
-import { ExpoSpeechTranscriberModuleEvents } from './ExpoSpeechTranscriber.types';
+// import { registerWebModule, NativeModule } from 'expo';
+// import { ExpoSpeechTranscriberModuleEvents } from './ExpoSpeechTranscriber.types';
 
-class ExpoSpeechTranscriberModule extends NativeModule<ExpoSpeechTranscriberModuleEvents> {
-  PI = Math.PI;
-  async setValueAsync(value: string): Promise<void> {
-    this.emit('onChange', { value });
-  }
-  hello() {
-    return 'Hello world! 👋';
-  }
-}
+// class ExpoSpeechTranscriberModule extends NativeModule<ExpoSpeechTranscriberModuleEvents> {
+//   PI = Math.PI;
+//   async setValueAsync(value: string): Promise<void> {
+//     this.emit('onChange', { value });
+//   }
+//   hello() {
+//     return 'Hello world! 👋';
+//   }
+// }
 
-export default registerWebModule(ExpoSpeechTranscriberModule, 'ExpoSpeechTranscriberModule');
+// export default registerWebModule(ExpoSpeechTranscriberModule, 'ExpoSpeechTranscriberModule');
